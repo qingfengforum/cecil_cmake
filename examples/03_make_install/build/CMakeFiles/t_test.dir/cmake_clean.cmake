ff2818d419c63d1695b6197d88ce977badbfaa05
yuyuan <yuyuan@nebula-link.com>
2020-01-07 15:30:59 +0800

    update examples

---
+++ examples/03_make_install/build/CMakeFiles/t_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t_test.dir/main.cpp.o"
+  "t_test.pdb"
+  "t_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/t_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/03_make_install/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "this is test cmake install command" << endl;
+    return 0;
+}
